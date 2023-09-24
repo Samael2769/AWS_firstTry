3225599c8512d69c6e786f7310c70edd1db0e998
Samael <jeremie.joannard@epitech.eu>
2023-09-25 05:24:22 +0900

    [MOD] js/config.js,
    new config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,19 @@
 window._config = {
+
     cognito: {
-        userPoolId: 'ap-northeast-2_ovEWI4CHy', // e.g. us-east-2_uXboG5pAb
+
+        userPoolId: 'ap-northeast-2_ovEWI4CHy', // e.g. us-east-2_uXboG5pAb         
+
         userPoolClientId: '517j4m9lbjtf7ja5hctkgf7qno', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-2' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+
+        region: 'ap-northest-2' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://7vxe8igz50.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
 };
